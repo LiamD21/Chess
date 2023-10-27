e511918bf656e2a170bf43131782973ce2859d80
LiamD21 <58191445+LiamD21@users.noreply.github.com>
2023-10-27 10:35:14 -0600

    Created a get move input function

--- src/MainGame.java
+++ src/MainGame.java
@@ -1,11 +1,15 @@
 import com.sun.tools.javac.Main;
 
+import java.util.Scanner;
+
 public class MainGame {
 
     private char turn;
+    private ChessBoard board;
 
     public MainGame(){
         turn = 'w';
+        board = new ChessBoard();
     }
 
     /**
@@ -20,8 +24,35 @@ public class MainGame {
         }
     }
 
-    public static void main(String[] args){
-        // create a new board object
+    private String[] getMove(){
+        Scanner sc = new Scanner(System.in);
+        String[] move = new String[3];
 
+        // Print move prompt
+        if (this.turn == 'w'){
+            System.out.println("Enter White's Move: ");
+        }
+        else if (this.turn == 'b'){
+            System.out.println("Enter Black's Move: ");
+        }
+
+        // get move from console
+        String input = sc.nextLine();
+
+        // interpret move
+        move[0] = input.substring(0, input.length() - 2);
+        move[1] = input.substring(input.length()-2, input.length()-1);
+        move[2] = input.substring(input.length()-1);
+
+        return move;
+    }
+
+    public static void main(String[] args) {
+        // create a new game
+        MainGame instance = new MainGame();
+        String[] move = instance.getMove();
+        System.out.println(move[0]);
+        System.out.println(move[1]);
+        System.out.println(move[2]);
     }
 }
