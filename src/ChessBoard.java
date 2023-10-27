1dfda00ec981cab846041c2ffd66fddc37b5e24d
LiamD21 <58191445+LiamD21@users.noreply.github.com>
2023-10-27 10:01:56 -0600

    Added ChessBoard class

---
+++ src/ChessBoard.java
@@ -0,0 +1,21 @@
+public class ChessBoard {
+
+    /**
+     * The chessboard. An array of characters representing pieces. Capital chars are black, lowercase is white
+     */
+    char[][] board;
+
+    /**
+     * Default constructor - Initializes the 2D chessboard array
+     */
+    public ChessBoard(){
+        board = new char[][]{{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+                {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+                {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+                {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+                {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+                {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},};
+    }
+}
