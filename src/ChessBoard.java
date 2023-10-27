aaddb354401eef22eacf76729586c3db4d30798f
LiamD21 <58191445+LiamD21@users.noreply.github.com>
2023-10-27 10:06:37 -0600

    Added a way to check the win condition on a chessboard

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -18,4 +18,33 @@ public class ChessBoard {
                 {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
                 {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},};
     }
+
+    /**
+     * Checks for a win on the current board
+     * @return A string for who won, white or black, or no for no win yet
+     */
+    public String checkWin(){
+        boolean whiteKing = false;
+        boolean blackKing = false;
+
+        // check the board for either king
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j < 8; j++){
+                if (this.board[i][j] == 'k'){
+                    whiteKing = true;
+                }
+                else if (this.board[i][j] == 'K'){
+                    blackKing = true;
+                }
+            }
+        }
+
+        if (whiteKing && !blackKing){
+            return "White";
+        }
+        else if (blackKing && !whiteKing){
+            return "Black";
+        }
+        return "No";
+    }
 }
